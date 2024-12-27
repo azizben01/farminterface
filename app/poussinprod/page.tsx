8d0010db5737a85b2f556de1aacacf0391125854
azizben01 <benazizsang1@gmail.com>
2024-12-27 19:18:07 +0200

    test

--- app/poussinprod/page.tsx
+++ app/poussinprod/page.tsx
@@ -58,7 +58,7 @@ const Poussinprod = () => {
 
     try {
       const response = await fetch(
-        "https://farmapi-jimn.onrender.com/chickproductionform",
+        "https://farmapi-jimn.onrender.com:5050/chickproductionform",
         {
           method: "POST",
           headers: {
