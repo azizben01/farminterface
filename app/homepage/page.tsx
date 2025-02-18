063cb75096cad98ab95ca45ac3b2b49eba9555d0
azizben01 <benazizsang1@gmail.com>
2025-02-18 15:50:27 +0200

    refinement

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -14,7 +14,7 @@ const Home = () => {
 
       <div className="relative space-y-6 bg-gray-100 bg-opacity-50 rounded-xl shadow-xl flex flex-col items-center justify-center w-11/12 sm:w-[600px] md:w-[700px] lg:w-[700px] xl:w-[800px] p-6 sm:p-8 md:p-10 lg:p-12">
         <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl font-bold text-custom-green sm:pb-10 md:pb-10 lg:pb-10 xl:pb-10">
-          Choisissez parmis l'une des pages suivantes pour continuer.
+          Choisissez parmis l&#39;une des pages suivantes pour continuer.
         </h1>
 
         <div className="flex flex-col justify-center gap-4 w-full">
